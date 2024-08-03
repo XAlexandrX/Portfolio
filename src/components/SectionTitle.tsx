38606deb3830912856d5fbc70970dada5882f681
Limarenko Aleksandr <Limarenko_a.a@mail.ru>
2024-08-03 23:09:23 +0300

    modified SectionTitle

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 
 export const SectionTitle = styled.h2`
-    color: red;
     text-align: center;
+    font-family: "Josefin Sans", "sans-serif";
+    font-weight: 600;
+    font-size: 36px;
+    letter-spacing: 5px;
+    color: #fff;
 `

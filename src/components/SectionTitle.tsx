6e3484c98c534d37594e8ab53d53c362c0eb074d
Limarenko Aleksandr <Limarenko_a.a@mail.ru>
2024-08-03 23:18:22 +0300

    modified SectionTitle

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { theme } from "../styles/Theme";
 
 
 export const SectionTitle = styled.h2`
@@ -8,4 +9,23 @@ export const SectionTitle = styled.h2`
     font-size: 36px;
     letter-spacing: 5px;
     color: #fff;
+    margin-bottom: 90px;
+
+    position: relative;
+
+
+    &::before {
+        content:"";
+        display: inline-block;
+        width: 55px;
+        height: 1px;
+        background-color: ${theme.colors.accent};
+
+        position: absolute;
+        left: 50%;
+        bottom: -30px;
+        transform: translateX(-50%);
+
+
+    }
 `

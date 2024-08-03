2a41f11f50ca5859220db5dd14192bd94a8562a5
Limarenko Aleksandr <Limarenko_a.a@mail.ru>
2024-08-03 23:03:24 +0300

    modified Skills

--- src/layout/section/skills/skills.tsx
+++ src/layout/section/skills/skills.tsx
@@ -1,32 +1,34 @@
 import styled from "styled-components"
 import { FlexWrapper } from "../../../components/FlexWrapper"
-import { Icon } from "../../../components/icon/Icon"
 import { SectionTitle } from "../../../components/SectionTitle"
 import { Skill } from "./skill/Skill"
+import { Container } from "../../../components/Container";
 
 export const Skills = () => {
     return (
         <StyledSkills>
-            <SectionTitle>My Skills</SectionTitle>
-            <FlexWrapper wrap={"wrap"} justify={"space-between"}>
-                <Skill iconId={"code"}
-                    title={"html5"} description={"lorem  lorem lorem"}/>
-                <Skill iconId={"css"} 
-                    title={"css3"} description={"lorem lorem lorem"}/>
-                <Skill iconId={"react"}
-                    title={"react"} description={"lorem lorem lorem"}/>
-                <Skill iconId={"typeScript"}
-                    title={"typescript"} description={"lorem  lorem lorem"}/>
-                <Skill iconId={"styledComponents"} 
-                    title={"styled components"} description={"lorem lorem lorem"}/>
-                <Skill iconId={"figma"}
-                    title={"web desing"} description={"lorem lorem lorem"}/>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>My Skills</SectionTitle>
+                <FlexWrapper wrap={"wrap"} justify={"space-between"}>
+                    <Skill iconId={"code"}
+                        title={"html5"} description={"lorem  lorem lorem"}/>
+                    <Skill iconId={"css"} 
+                        title={"css3"} description={"lorem lorem lorem"}/>
+                    <Skill iconId={"react"}
+                        title={"react"} description={"lorem lorem lorem"}/>
+                    <Skill iconId={"typeScript"}
+                        title={"typescript"} description={"lorem  lorem lorem"}/>
+                    <Skill iconId={"styledComponents"} 
+                        title={"styled components"} description={"lorem lorem lorem"}/>
+                    <Skill iconId={"figma"}
+                        title={"web desing"} description={"lorem lorem lorem"}/>
+                </FlexWrapper>
+            </Container>
+           
         </StyledSkills>
     );
 };
 
 const StyledSkills = styled.section`
-    background-color: #9c9df5;
-    min-height: 100vh;
+
 `

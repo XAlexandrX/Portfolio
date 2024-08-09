bf5b7081bc433f55f39e7799f84b26b600ee7be0
Limarenko Aleksandr <Limarenko_a.a@mail.ru>
2024-08-09 23:34:47 +0300

    modified Works

--- src/layout/section/works/Works.tsx
+++ src/layout/section/works/Works.tsx
@@ -6,22 +6,25 @@ import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Work } from './work/Work';
 import socialImg from './../../../assets/images/Work.png';
 import TimerImg from './../../../assets/images/Work1.png';
+import { Container } from '../../../components/Container';
 
 const worksItems =["All", "Landing page", "Rect", "Spa" ]
 
 export const Works = () => {
     return (
         <StyledWorks>
-            <SectionTitle>My Works</SectionTitle>
-            <HeaderMenu menuItems={worksItems}/>
-            <FlexWrapper justify={"space-around"}>
-                <Work title={"Social Network"} 
-                      src={socialImg}
-                      text={"Text text text"}/>
-                <Work title={"Timer"} 
-                    src={TimerImg}
-                    text={"Text2 Text2 Text2"}/>
-            </FlexWrapper>
+            <Container>
+                <SectionTitle>My Works</SectionTitle>
+                <HeaderMenu menuItems={worksItems}/>
+                <FlexWrapper justify={"space-between"}>
+                    <Work title={"Social Network"} 
+                        src={socialImg}
+                        text={"Text text text"}/>
+                    <Work title={"Timer"} 
+                        src={TimerImg}
+                        text={"Text2 Text2 Text2"}/>
+                </FlexWrapper>
+            </Container>
         </StyledWorks>
 
     )

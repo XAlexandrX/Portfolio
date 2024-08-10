849fad9c794617c1df77f0bc5efe2ac83ccf03bf
Limarenko Aleksandr <Limarenko_a.a@mail.ru>
2024-08-10 14:06:25 +0300

    add component Link

--- src/layout/section/works/tabMenu/TabMenu.tsx
+++ src/layout/section/works/tabMenu/TabMenu.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { theme } from '../../../../styles/Theme';
+import { Link } from '../../../../components/Link';
 
 
 
@@ -32,33 +33,5 @@ const StyledTabMenu = styled.nav `
 const ListItem = styled.li `
     position: relative;
     z-index: 0;
-`
-
-const Link = styled.a `
-    font-weight: 400;
-    font-size: 14px;
-    letter-spacing: 1px;
-    text-transform: uppercase;
-    padding: 10px;
-
-    &:hover {
-        &::before {
-            height: 10px;
-        }
-
-    }
-
-    &::before {
-        content:"";
-        display:inline-block;
-        /* height: 10px; */
-        background-color: ${theme.colors.accent};
-
-        position: absolute;
-        bottom: -5px;
-        left: 0;
-        right: 0;
-        z-index: -1;
-    }
 
 `
